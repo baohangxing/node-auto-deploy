dea59cacc4ce0044df2ed2ade5ab74d5cf94d6c9
baohangxing <2292398086@qq.com>
2021-11-21 04:12:39 +0800

    [feat] public project

--- app.js
+++ app.js
@@ -28,38 +28,38 @@ const server = http.createServer((req, res) => {
             return res.end('格式错误');
         }
         try {
-            // if (
-            //     queryData.ref === 'refs/heads/main' &&
-            //     queryData.repository &&
-            //     queryData.repository.full_name
-            // ) {
-            let isAddQueueFlag = taskQueue.autoDeploy(
-                queryData.repository.full_name,
-                {
-                    beforeHooks: [
-                        function (task) {
-                            const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n开始部署...\n`;
-                            messagePush(text);
-                        },
-                    ],
-                    deploydHooks: [
-                        function (task, timeCost) {
-                            const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n状态：部署成功(${timeCost}s)\n`;
-                            messagePush(text);
-                        },
-                    ],
-                    errorHooks: [
-                        function (task, error) {
-                            const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n状态：部署错误(${error})\n`;
-                            messagePush(text);
-                        },
-                    ],
-                }
-            );
-            res.end(isAddQueueFlag ? '部署任务加入队列' : '部署任务开始');
-            // } else {
-            //     return res.end('格式错误');
-            // }
+            if (
+                queryData.ref === 'refs/heads/main' &&
+                queryData.repository &&
+                queryData.repository.full_name
+            ) {
+                let isAddQueueFlag = taskQueue.autoDeploy(
+                    queryData.repository.full_name,
+                    {
+                        beforeHooks: [
+                            function (task) {
+                                const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n开始部署...\n`;
+                                messagePush(text);
+                            },
+                        ],
+                        deploydHooks: [
+                            function (task, timeCost) {
+                                const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n状态：部署成功(${timeCost}s)\n`;
+                                messagePush(text);
+                            },
+                        ],
+                        errorHooks: [
+                            function (task, error) {
+                                const text = `项目：${task.name}\n提交人：${queryData.head_commit.author.username}\n提交信息：${queryData.head_commit.message}\n状态：部署错误(${error})\n`;
+                                messagePush(text);
+                            },
+                        ],
+                    }
+                );
+                res.end(isAddQueueFlag ? '部署任务加入队列' : '部署任务开始');
+            } else {
+                return res.end('格式错误');
+            }
         } catch (error) {
             res.end(error.message + '\n\n' + error.stack);
         }
